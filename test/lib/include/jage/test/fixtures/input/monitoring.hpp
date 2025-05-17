db5a2db9dc6f002b2739e3a4efdcb9a7389d5dfb
jlye <justinmlye@gmail.com>
2025-05-17 15:11:04 -0500

    Make monitor test typed test so that mouse monitoring change just be added to the test types

--- test/lib/include/jage/test/fixtures/input/monitoring.hpp
+++ test/lib/include/jage/test/fixtures/input/monitoring.hpp
@@ -16,6 +16,14 @@ protected:
   TMonitor monitor{mock_driver};
   using button_type = TButton;
   static constexpr auto null_callback = [](const auto &) -> void {};
+  static_assert(std::to_underlying(TButton::END) >= 2);
+  static constexpr auto button0 =
+      static_cast<TButton>(static_cast<std::underlying_type_t<TButton>>(0UZ));
+
+  static constexpr auto button1 =
+      static_cast<TButton>(static_cast<std::underlying_type_t<TButton>>(1UZ));
+  static constexpr auto button2 =
+      static_cast<TButton>(static_cast<std::underlying_type_t<TButton>>(2UZ));
 
   auto expect_call_to_is_down(const TButton key,
                               const std::uint8_t times = 1U) -> void {
