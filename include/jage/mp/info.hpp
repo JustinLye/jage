309bfcfda8a4e3ce2942f0d23f8cf865e05cc6ad
jlye <justinmlye@gmail.com>
2025-05-17 10:52:46 -0500

    fix issues with clang compiler

--- include/jage/mp/info.hpp
+++ include/jage/mp/info.hpp
@@ -3,8 +3,11 @@
 namespace jage::mp {
 template <auto id> struct info {
 #pragma GCC diagnostic push
+#if !defined(__clang__)
 #pragma GCC diagnostic ignored "-Wnon-template-friend"
+#endif
   constexpr auto friend get(info);
+
 #pragma GCC diagnostic pop
 };
 } // namespace jage::mp
