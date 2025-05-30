b94f341452ec237e4d546bffb43f588fd6b787ef
Justin <justinmlye@gmail.com>
2025-05-30 08:48:38 -0500

    ♻️  [test] Simplify template instantiation of monitor typed test suit… (#13)
    ♻️  [test] Simplify template instantiation of monitor typed test suite by using monitor_target_t instead of pair

--- test/lib/include/jage/test/fixtures/input/button/monitoring.hpp
+++ test/lib/include/jage/test/fixtures/input/button/monitoring.hpp
@@ -9,23 +9,24 @@
 
 namespace jage::test::fixtures::input::button {
 
-template <class TMonitor, class TButton> struct monitoring : testing::Test {
+template <class TMonitor> struct monitoring : testing::Test {
 protected:
-  mocks::input::button::callback<TButton> mock_callback{};
-  mocks::input::button::driver<TButton> mock_driver{};
+  using button_type_t = typename TMonitor::monitor_target_t;
+  mocks::input::button::callback<button_type_t> mock_callback{};
+  mocks::input::button::driver<button_type_t> mock_driver{};
   TMonitor monitor{mock_driver};
-  using button_type = TButton;
+  using button_type = button_type_t;
   static constexpr auto null_callback = [](const auto &) -> void {};
-  static_assert(std::to_underlying(TButton::END) >= 2);
-  static constexpr auto button0 =
-      static_cast<TButton>(static_cast<std::underlying_type_t<TButton>>(0UZ));
+  static_assert(std::to_underlying(button_type_t::END) >= 2);
+  static constexpr auto button0 = static_cast<button_type_t>(
+      static_cast<std::underlying_type_t<button_type_t>>(0UZ));
 
-  static constexpr auto button1 =
-      static_cast<TButton>(static_cast<std::underlying_type_t<TButton>>(1UZ));
-  static constexpr auto button2 =
-      static_cast<TButton>(static_cast<std::underlying_type_t<TButton>>(2UZ));
+  static constexpr auto button1 = static_cast<button_type_t>(
+      static_cast<std::underlying_type_t<button_type_t>>(1UZ));
+  static constexpr auto button2 = static_cast<button_type_t>(
+      static_cast<std::underlying_type_t<button_type_t>>(2UZ));
 
-  auto expect_call_to_is_down(const TButton key,
+  auto expect_call_to_is_down(const button_type_t key,
                               const std::uint8_t times = 1U) -> void {
     if (times > 0) {
       EXPECT_CALL(mock_driver, is_down(key))
