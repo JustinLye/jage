4d323b9d78917d625c51844b544bf44c21142da0
jlye <justinmlye@gmail.com>
2026-01-10 11:28:23 +0000

    🧪 Convert window tests to GUnit
    Problem:
    - Window unit tests used mixed GTest style vs existing GUnit conventions
    
    Solution:
    - Replace TEST_F with GTEST/SHOULD and use GUnit EXPECT helpers

--- test/unit/jage/window_test.cpp
+++ test/unit/jage/window_test.cpp
@@ -1,28 +1,29 @@
 
 #include <jage/test/fixtures/window_test.hpp>
 
+#include <GUnit.h>
 #include <gmock/gmock.h>
-#include <gtest/gtest.h>
 
 using jage::test::fixtures::window_test;
 
-TEST_F(window_test, should_check_driver_if_window_should_be_closed) {
-  EXPECT_CALL(driver, should_close()).WillOnce(testing::Return(true));
-  EXPECT_TRUE(window.should_close());
-}
+GTEST(window_test) {
+  SHOULD("Check driver if window should be closed") {
+    EXPECT_CALL(driver, should_close()).WillOnce(testing::Return(true));
+    EXPECT(window.should_close());
+  }
 
-TEST_F(window_test,
-       should_return_value_driver_returns_for_window_should_be_closed) {
-  EXPECT_CALL(driver, should_close()).WillOnce(testing::Return(false));
-  EXPECT_FALSE(window.should_close());
-}
+  SHOULD("Return the driver value when checking if window should be closed") {
+    EXPECT_CALL(driver, should_close()).WillOnce(testing::Return(false));
+    EXPECT(not window.should_close());
+  }
 
-TEST_F(window_test, should_poll_driver) {
-  EXPECT_CALL(driver, poll()).Times(1);
-  window.poll();
-}
+  SHOULD("Poll the driver") {
+    EXPECT_CALL(driver, poll()).Times(1);
+    window.poll();
+  }
 
-TEST_F(window_test, should_call_render_on_the_driver) {
-  EXPECT_CALL(driver, render()).Times(1);
-  window.render();
+  SHOULD("Call render on the driver") {
+    EXPECT_CALL(driver, render()).Times(1);
+    window.render();
+  }
 }
