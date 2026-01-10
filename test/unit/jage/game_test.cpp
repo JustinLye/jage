6c08a3908b26850a31275b32059b417719a36f90
jlye <justinmlye@gmail.com>
2026-01-09 20:51:29 -0600

    Set default game test expectations on fixture setup
    Problem:
    - Game loop tests required verbose gMock expectations for unrelated calls
    - Boilerplate expectations obscured the behavior each test was intended to verify
    
    Solution:
    - Add default ON_CALL behavior in the game test fixture to relax unused mock interactions
    - Remove unnecessary Times(0) expectations from game loop tests to keep them focused

--- test/unit/jage/game_test.cpp
+++ test/unit/jage/game_test.cpp
@@ -10,8 +10,6 @@ using jage::test::fixtures::game_test;
 TEST_F(game_test,
        should_check_if_window_should_not_poll_if_window_should_close_on_loop) {
   EXPECT_CALL(window_driver, should_close()).WillOnce(testing::Return(true));
-  EXPECT_CALL(window_driver, poll()).Times(0);
-  EXPECT_CALL(window_driver, render()).Times(0);
   game.loop();
 }
 
@@ -26,15 +24,6 @@ TEST_F(game_test, should_render_and_poll_window_while_window_is_not_closed) {
 
 TEST_F(game_test, should_not_poll_input_controller_if_window_is_closed) {
   EXPECT_CALL(window_driver, should_close()).WillOnce(testing::Return(true));
-  EXPECT_CALL(input_driver,
-              keyboard_is_down(jage::input::keyboard::keys::escape))
-      .Times(0);
-  EXPECT_CALL(input_driver,
-              mouse_is_down(jage::input::mouse::buttons::middle_click))
-      .Times(0);
-  EXPECT_CALL(input_driver, cursor_position()).Times(0);
-  EXPECT_CALL(window_driver, poll()).Times(0);
-  EXPECT_CALL(window_driver, render()).Times(0);
   auto &controller = game.input();
   controller.keyboard().monitor_input(jage::input::keyboard::keys::escape);
   controller.mouse().monitor_input(jage::input::mouse::buttons::middle_click);
