f725b5592ba9731f848fc36ce2784a69d6aa26b2
jlye <justinmlye@gmail.com>
2026-02-16 19:02:18 +0000

    🔥 Remove unused code
    Problem:
    - Input monitoring, game and window controllers are not part of the current JAGE design and cause confusion
    
    Solution:
    - Delete unused code

--- test/lib/include/jage/test/mocks/input/button/driver.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <gmock/gmock.h>
-
-namespace jage::test::mocks::input::button {
-template <class TButton> struct driver {
-  MOCK_METHOD(bool, is_down, (const TButton), (const));
-};
-} // namespace jage::test::mocks::input::button
